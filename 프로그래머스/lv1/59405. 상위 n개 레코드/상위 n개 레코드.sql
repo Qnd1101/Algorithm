89301a8d4871769291f78274d70061d65eec4767
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-04-19 10:50:35 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/lv1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,5 +1,8 @@
 -- 코드를 입력하세요
-SELECT name 
-from animal_ins
-order by datetime
-limit 1;
+-- 선생님 풀이
+-- select name
+-- from (select * from animal_ins order by datetime)
+-- where rownum = 1;
+SELECT NAME
+FROM ANIMAL_INS
+WHERE DATETIME = (SELECT MIN(DATETIME) FROM ANIMAL_INS);

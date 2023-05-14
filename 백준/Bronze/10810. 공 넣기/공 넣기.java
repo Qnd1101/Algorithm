2018467d1613e732e6598e338fc4ef2e6a76e273
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-05-14 14:24:31 +0900

    [Bronze III] Title: 공 넣기, Time: 248 ms, Memory: 18188 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -3,13 +3,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        // 도현이의 총 바구니 수(N)
+        // 도현이의 총 바구니 수 N 선언
         int n = sc.nextInt();
         // 도현이가 앞으로 M번 공을 넣으려고 한다.
         int M = sc.nextInt();
-
-        // 도현이의 총 바구니 수를 출력하기 위한 배열
-        int[] N = new int[n];
+        // 바구니 배열
+        int[] arr = new int[n];
 
         for(int i = 0; i < M; i++){
             // I번 바구니부터,
@@ -21,15 +20,14 @@ public class Main {
             
             // 배열은 0번째부터이므로 'I - 1'을 해준다.
             for(int j = I - 1; j < J; j++){
-                N[j] = K;
+                arr[j] = K;
             }
         }
 
         // 출력
-        for(int i = 0; i < N.length; i++){
-            System.out.printf("%d ", N[i]);
+        for(int i = 0; i < arr.length; i++){
+            System.out.printf("%d ", arr[i]);
         }
-
         sc.close();
     }
 }

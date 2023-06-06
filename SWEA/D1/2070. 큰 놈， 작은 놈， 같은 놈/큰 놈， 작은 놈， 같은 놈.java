58ea13c71be485d4f24199036a5f0041adf50720
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-06-06 16:13:09 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 133 ms, Memory: 20,524 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for(int i = 1; i <= T; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			if(a == b) {
+				System.out.printf("#%d %s\n", i, "=");
+			} else if (a > b) {
+				System.out.printf("#%d %s\n", i, ">");
+			} else {
+				System.out.printf("#%d %s\n", i, "<");
+			}
+		}
+		sc.close();
+	}
+}

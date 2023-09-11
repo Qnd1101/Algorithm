20282949d7b7091f60725838c3306f0f9449d240
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-09-12 08:23:07 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,32 +1,41 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] ar = new int[N];
-        for(int i = 0; i < N; i++){
-            ar[i] = sc.nextInt();
-        }
-        int ij = 0;
-        int ijk = 0;
-        int max = 0;
-
-        for (int i = 0; i < ar.length; i++){
-            ij = 0;
-            for (int j = i+1; j < ar.length; j++){
-                ij = ar[i] + ar[j];
-                for(int k = j+1; k < ar.length; k++){
-                    ijk = ij + ar[k];
-                    if(ijk <= M && ijk > max){
-                        max = ijk;
-                    }
-                }
-            }
-        }
-        System.out.println(max);
-    }
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] inputNum = br.readLine().split(" ");
+		
+		int n = Integer.parseInt(inputNum[0]);
+		int m = Integer.parseInt(inputNum[1]);
+		
+		String[] str = br.readLine().split(" ");
+		
+		int[] arr = new int[n];
+		for(int i = 0; i < str.length; i++) {
+			arr[i] = Integer.parseInt(str[i]);
+		}
+		
+		
+		int ij = 0;
+		int ijk = 0;
+		int max = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			ij = 0;
+			for(int j = i+1; j < arr.length; j++) {
+				ij = arr[i] + arr[j];
+				for(int k = j+1; k < arr.length; k++) {
+					ijk = ij + arr[k];
+					if(m >= ijk && ijk > max) {
+						max = ijk;
+					}
+				}
+			}
+		}
+		
+		System.out.println(max);
+	}
+	
 }

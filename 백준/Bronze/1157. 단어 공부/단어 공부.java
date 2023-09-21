ccc793d6dea7f8df5c56c1416b555efe95b0fc5f
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-09-21 20:23:47 +0900

    [Bronze I] Title: 단어 공부, Time: 448 ms, Memory: 28628 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -18,21 +18,21 @@ public class Main {
         }
         // max의 초깃값을 0으로 해버리면 밑에 for 문에서 else if 문에 적합하게 되어 -1을 저장.
         // 문자의 빈도수를 정확하게 하기 위한것도 있음.
-        int max = -1;
-		char ch = '?';
+        int max = 0;
+	    char ch = '?';
         
-		for (int i = 0; i < 26; i++) { // 알파벳 개수만큼 반복
+	    for (int i = 0; i < 26; i++) { // 알파벳 개수만큼 반복 
 
-			if (intArr[i] > max) { // 조건에 성립할 경우 
-				max = intArr[i]; // 최댓값을 저장
-				ch = (char) (i + 65); // 대문자를 출력해야하기 때문에 i + 65
-			} 
-			else if (intArr[i] == max) { // 조건에 성립할 경우 
-				ch = '?'; // max와 같을경우
-			}
-		}
+		    if (intArr[i] > max) { // 조건에 성립할 경우 
+		    	max = intArr[i]; // 최댓값을 저장
+		    	ch = (char) (i + 65); // 대문자를 출력해야하기 때문에 i + 65
+		    } 
+		    else if (intArr[i] == max) { // 조건에 성립할 경우 
+		    	ch = '?'; // max와 같을경우
+		    }
+	    } 
 
         System.out.println(ch);
         sc.close();
-    }
+    }   
 }

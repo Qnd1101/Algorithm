b7a2410586c39c1a28371202f29f76816107467b
김민준 <107795830+Qnd1101@users.noreply.github.com>
2023-11-18 23:29:57 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 312 ms, Memory: 20772 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,12 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.EmptyStackException;
 
 /*
 
 입력 :
 So when I die (the [first] I will see in (heaven) is a score list).
-([ first in ] ( first out ).
+[ first in ] ( first out ).
 Half Moon tonight (At least it is better than no Moon at all].
 A rope may form )( a trail in a maze.
 Help( I[m being held prisoner in a fortune cookie factory)].
@@ -34,46 +35,27 @@ public class Main {
 
             if (inputStr.equals(".")) break; // '.' 이라는 문자를 만나면? 멈춘다.
 
-            MinStack stack = new MinStack(); // 스택 구현.
-            String[] strArr = inputStr.split("");
+            MinStack<Character> stack = new MinStack<>(10);
+            char[] charArray = inputStr.toCharArray();
 
-//            for(String str : strArr){
-//                if(str.equals("(")){
-//                    stack.push("(");
-//                }else if(str.equals("[")){
-//                    stack.push("[");
-//                }else if(str.equals(")")){
-//                    if (stack.peek().equals("(")) {
-//                        stack.pop();
-//                    } else {
-//                        stack.push(")");
-//                    }
-//                }else if(str.equals("]")){
-//                    if (stack.peek().equals("[")) {
-//                        stack.pop();
-//                    }else {
-//                        stack.push("]");
-//                    }
-//                }
-//            }
-            for (String str : strArr) {
-                switch (str) {
-                    case "(":
-                    case "[":
-                        stack.push(str);
+            for (char ch : charArray) {
+                switch (ch) {
+                    case '(':
+                    case '[':
+                        stack.push(ch);
                         break;
-                    case ")":
-                        if (stack.peek().equals("(")) { // 가장 위에 있는 문자가 "(" 인 경우엔 pop() 메서드를 호출하고?
+                    case ')':
+                        if (!stack.isEmpty() && stack.peek() == '(') { // 스택이 비어있고 가장 위에 있는 문자가 "(" 인 경우엔 pop() 메서드를 호출
                             stack.pop();
-                        } else { // 아니면 push() 메서드를 호출한다.
-                            stack.push(str);
+                        } else {
+                            stack.push(ch);
                         }
                         break;
-                    case "]":
-                        if (stack.peek().equals("[")) { // 가장 위에 있는 문자가 "[" 인 경우엔 pop() 메서드를 호출하고?
+                    case ']':
+                        if (!stack.isEmpty() && stack.peek() == '[') { // 스택이 비어있고 가장 위에 있는 문자가 "[" 인 경우엔 pop() 메서드를 호출
                             stack.pop();
-                        } else { // 아니면 push() 메서드를 호출한다.
-                            stack.push(str);
+                        } else {
+                            stack.push(ch);
                         }
                         break;
                 }
@@ -84,49 +66,66 @@ public class Main {
             } else { // 스택에 값이 들어있는 경우 : no
                 System.out.println("no");
             }
+
+            stack.clear();
         }
     }
 }
-class MinStack {
-    private int top;
-    private String[] stack;
 
-    public MinStack() {
-        this.top = -1;
-        this.stack = new String[10];
+class MinStack<E> {
+    private int size;
+    private int index;
+    private Object[] stackArray;
+
+    public MinStack(int inputSize) {
+        size = inputSize;
+        stackArray = new Object[size];
     }
 
     // 스택에 값을 추가하는 메서드
-    public void push(String value) {
-        if(top == stack.length - 1) { // 스택이 넘쳤을 경우에
-            String[] newStack = new String[stack.length * 2]; // 배열이 가득 찼을 경우 크기를 현재 크기의 2배로 확장한다.
-            for(int i = 0; i < top + 1; i++){
-                newStack[i] = stack[i];
-            }
-            stack = newStack; // 새로운 배열로 업데이트(배열의 길이 + 내용 전부 복사)
-        }
-        stack[++top] = value;
-
+    public void push(E element) {
+        if(index == size) arrayLengthCompare();
+        stackArray[index++] = element; // 현재 위치에 요소를 넣는다.
     }
 
     // 스택에서 값을 제거하고 반환하는 메서드
-    public String pop() {
+    public E pop() {
         if (!isEmpty()) {
-            return stack[top--]; // 가장 위에 있는 문자 반환 및 top 감소
+            return (E) stackArray[--index];
         }
-        return "underflow";
+        throw new EmptyStackException();
     }
 
     // 스택이 비어있는지 확인하는 메서드
     public boolean isEmpty() {
-        return top == -1;
+        return index == 0;
     }
 
     // 스택의 맨 위의 값을 확인하는 메서드
-    public String peek() {
-        if (!isEmpty()) { // 스택이 존재 한다면?
-            return stack[top];  // 가장 위에 있는 값을 반환
+    public E peek() {
+        if (!isEmpty()) {
+            return (E) stackArray[index - 1];
         }
-        return ""; // 존재하지 않을 시에는 공백을 반환
+        throw new EmptyStackException();
+    }
+
+    // 스택의 사이즈를 확인하는 메서드
+    public int size() {
+        return index;
+    }
+
+    // 스택의 초기 상태로 돌아가는 메서드
+    public void clear() {
+        index = 0;
+        stackArray = new Object[size];
+    }
+
+    // 스택의 길이를 비교하여 배열의 크기를 늘리는 메서드
+    private void arrayLengthCompare() {
+            int newStackArrayLength = stackArray.length * 2;
+            size = newStackArrayLength;
+            stackArray = java.util.Arrays.copyOf(stackArray, newStackArrayLength); // Arrays.copyOf() 메서드는 원본 배열의 요소를 새로운 배열에 복사하는 메서드이다.
+
     }
 }
+

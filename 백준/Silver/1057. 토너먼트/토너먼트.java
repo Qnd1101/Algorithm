e1c63cadd7ff2f7ee8c29a021c51ed5dcfd64553
김민준 <107795830+Qnd1101@users.noreply.github.com>
2025-04-07 14:16:54 +0900

    [Silver IV] Title: 토너먼트, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,42 +8,17 @@ public class Main {
         int n = Integer.parseInt(str[0]);
         int a = Integer.parseInt(str[1]);
         int b = Integer.parseInt(str[2]);
-        
-        List<Integer> num = new ArrayList<>();
-        
-        int result = 0;
+
         int round = 0;
-        
-        // 리스트에 n개의 번호를 지정
-        for(int i = 0; i < n; i++) {
-        	num.add(i+1);
-    	}
-        
-        // 0 일때만 돌아감
-        while(result == 0) {
-        	round += 1;
-        	
-        	for(int i = 0; i < num.size()-1; i++) {
-        		if ((num.get(i) == a || num.get(i) == b) &&
-        			(num.get(i + 1) == a || num.get(i + 1) == b)) {
-        			result = round;	
-        			break;
-        		} else if (num.get(i) == a || num.get(i) == b) {
-        			num.remove(i+1);
-        		} else if(num.get(i + 1) == a || num.get(i + 1) == b) {
-        			num.remove(i);
-        		} else {
-        			num.remove(i);
-        		}
-        	}
-        	
-        	if(num.size() == 1) {
-        		round = -1;
-        	}
+
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            round++;
         }
-        
-        System.out.println(result);
-        
-        sc.close(); // Scanner 닫기
+
+        System.out.println(round);
+
+        sc.close();
     }
 }
